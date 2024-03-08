b2d70439f2ce09db7ed1951c45a02b7ea82684dd
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-03-08 18:52:27 +0900

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -5,5 +5,5 @@ uint32_t reverse(char* filename){
        char n[5] = fread(n,sizeof(uint32_t),4,fp);
        uint32_t *num = reinterpret_cast<uint32_t *>(n);
        fclose(fp);
-       return htonl(num);
+       return htonl(*num);
 }
